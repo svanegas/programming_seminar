28c155ab6dc00a403ab2b508c4933633ceb28fd0
EstebanFS <seforonda@gmail.com>
2015-08-30 21:48:52 -0500

    Adding new topics, initialize and more tips

---
+++ Presentations/XXIX Colombian Programming Contest tips/src/initArrays.cpp
@@ -0,0 +1,9 @@
+map <string, int> m;
+set <int> s;
+queue <int> q;
+vector <int> v;
+int arraySize = 100;
+int arr[arraySize];
+for(int i = 0; i < arraySize; ++i) arr[i] = 0;
+memset(arr, 0, sizeof(arr)); //This method only allow values of 0 and -1
+memset(arr, -1, sizeof(arr)); //in arrays of integers.
