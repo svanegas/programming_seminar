bbea2aa0f62350cdf78e3bb6c140905c04fdce89
Esteban Foronda Sierra <seforonda@gmail.com>
2016-11-10 21:11:04 -0500

    Fixing code visualization

--- Just Code It. programming handbok/src/build-BT-from-pre-in.cpp
+++ Just Code It. programming handbok/src/build-BT-from-pre-in.cpp
@@ -10,9 +10,9 @@ string reconstruct(int stpre, int epre, int stin, int ein) {
       break;
     }
   }
-  string left = reconstruct(stpre + 1, stpre + (pos - stin), stin,
-                                                          pos - 1);
-  string right = reconstruct(stpre + (pos - stin) + 1, epre, pos + 1,
-                                                                ein);
+  string left = reconstruct(stpre + 1, stpre + (pos - stin),
+                            stin, pos - 1);
+  string right = reconstruct(stpre + (pos - stin) + 1, epre,
+                             pos + 1, ein);
   return (left + right) + currRoot;
 }
